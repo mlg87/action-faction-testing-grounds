31164ea5d4505a13b84356a85cf7105e9f682c2d
Mason Goetz <masonlgoetz@gmail.com>
2020-08-14 16:03:27 -0600

    add some shit

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const { WebClient } = require("@slack/web-api");
-const web = new WebClient();
+require("dotenv").config();
+const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 const conversationId = "G01922M7EG4";
 
 (async () => {
   // See: https://api.slack.com/methods/chat.postMessage
-  const res = await web.chat.postMessage({
-    channel: conversationId,
-    text: `Hello there <@UMK3ZJMLN>`,
-  });
+  // const res = await web.chat.postMessage({
+  //   channel: conversationId,
+  //   text: `Hello there <@UMK3ZJMLN>`,
+  // });
 
-  // `res` contains information about the posted message
-  console.log("Message sent: ", res);
+  // // `res` contains information about the posted message
+  // console.log("Message sent: ", res);
 
-  const reactionRes = await web.reactions.add({
+  // const reactionRes = await web.reactions.add({
+  //   channel: conversationId,
+  //   timestamp: res.ts,
+  //   name: "thumbsup",
+  // });
+  const res = await web.reactions.get({
     channel: conversationId,
-    timestamp: res.ts,
-    name: "thumbsup",
+    timestamp: "1597439544.023300",
   });
+  console.log("res.message.reactions", res.message.reactions);
 })();
